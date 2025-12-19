19ba23bc62dddfce4c3f7ac21047172605139180
Katrina <erozhenuk@gmail.com>
2025-12-19 09:13:15 +0300

    Добавлена структура Report для программы конференции

---
+++ main-project/conference_report.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+struct Report
+{
+    int startMinutes;
+    int endMinutes;
+    std::string surname;
+    std::string name;
+    std::string patronymic;
+    std::string topic;
+};
