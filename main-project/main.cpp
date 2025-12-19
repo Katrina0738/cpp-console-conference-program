f74da4802a1ba5050ed77920a6ea25c60c5b61ea
Katrina <erozhenuk@gmail.com>
2025-12-19 05:34:07 +0300

    Создана заготовка проекта

---
+++ main-project/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Лабораторная работа №8\n";
+    std::cout << "Вариант 2 — Программа конференции\n";
+    std::cout << "Студент: Иванов Иван\n";
+    return 0;
+}
