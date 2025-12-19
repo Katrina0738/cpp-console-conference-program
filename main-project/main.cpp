43d2da389f4fa9a6ed096ae4a4e8f8cdd72e8eb7
Katrina <erozhenuk@gmail.com>
2025-12-19 09:41:26 +0300

    Подключены структуры и добавлено открытие файла data.txt

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main() {
+#include "constants.h"
+#include "conference_report.h"
+
+using namespace std;
+
+int main()
+{
     setlocale(LC_ALL, "Russian");
-    std::cout << "Лабораторная работа №8\n";
-    std::cout << "Вариант 2 — Программа конференции\n";
-    std::cout << "Студент: Иванов Иван\n";
+    cout << "Лабораторная работа №8" << endl;
+    cout << "Вариант 2. Программа конференции" << endl;
+    cout << "Автор: Katrina" << endl << endl;
+
+    ifstream file("data.txt");
+
+    if (!file.is_open())
+    {
+        cout << "Не удалось открыть файл data.txt" << endl;
+        return 1;
+    }
+
+    cout << "Файл data.txt успешно открыт." << endl;
+
+    file.close();
     return 0;
 }
